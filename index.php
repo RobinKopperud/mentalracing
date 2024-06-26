488e41d7306be523165710673dfa1b0796374c59
RobinKopperud <Robin.kopperud@gmail.com>
2024-06-26 18:16:38 +0200

    ss

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ error_reporting(E_ALL);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mental Racing Team</title>
     <link rel="stylesheet" href="style.css?v=3.0">
+    <link rel="stylesheet" href="countdown.css?v=2.0">
     <link rel="stylesheet" href="css/news.css?v=2.0">
     <link rel="stylesheet" href="mobile.css?v=3.0" media="screen and (max-width: 768px)">
 </head>
@@ -89,8 +90,8 @@ error_reporting(E_ALL);
         <p>&copy; 2024 Mental Racing Team. Alle rettigheter reservert.</p>
     </footer>
 
-    <script src="script.js"></script>
-    <script src="nextrace.js"></script>
+    <script src="scripts/script.js"></script>
+    <script src="script/nextrace.js"></script>
     <script src="scripts/mail.js"></script>
     <script src="scripts/news.js"></script>
 </body>
